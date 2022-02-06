f31b10006694b720bfa4d41136aee62391f67fbf
tekitems <97388845+tekitems@users.noreply.github.com>
2022-02-06 17:24:53 +0100

    ulteriore aggiornamento pagina

---
+++ script/script.js
@@ -0,0 +1,30 @@
+var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl2 = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+tl.from('coming_soon', .5, {x:200, opacity: 0});
+
+tl2.from("#box", 1, {opacity: 0, scale: 0});
+tl2.to("#box", .5, {left: "20%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".sticky",
+            triggerHook: "onLeave",
+            duration: "100%"
+})
+  .setPin(".sticky")
+  .setTween(tl)
+		.addTo(controller);
+
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "blockquote"
+})
+  .setTween(tl2)
+		.addTo(controller);
+
+
+function updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+  tl.progress();
+  console.log(tl.progress());
+}
